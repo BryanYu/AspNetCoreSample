3fab57cab491055ba8bba4a89a0c3e49c696b6b7
BryanYu <d58526@gmail.com>
2019-10-15 16:13:08 +0800

    Add DbContext

--- AspNetCoreSample/AspNetCoreWebAPI/Startup.cs
+++ AspNetCoreSample/AspNetCoreWebAPI/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetCoreWebAPI.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +11,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace AspNetCoreWebAPI
 {
@@ -25,6 +28,8 @@ namespace AspNetCoreWebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(opt =>
+                opt.UseInMemoryDatabase("TodoList"));
             services.AddControllers();
         }
 
