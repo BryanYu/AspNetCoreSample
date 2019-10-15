52b954c8cf659272eb2cd5d746e31704ed666130
BryanYu <d58526@gmail.com>
2019-10-15 15:30:42 +0800

    Blazor Todo list

---
+++ AspNetCoreSample/AspNetCoreBlazor/TodoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreBlazor
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+
+        public bool IsDone { get; set; }
+    }
+}
