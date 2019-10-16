879258293942328ba03ed75cf4582bfd50c0cbef
BryanYu <d58526@gmail.com>
2019-10-16 13:46:52 +0800

    gRPC Sample

---
+++ AspNetCoreSample/AspNetCoregRPCClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using AspNetCoregRPC;
+using Grpc.Net.Client;
+
+namespace AspNetCoregRPCClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.CreateBookAsync(
+                new BookRequest() {Name = "123", Price = 2.05});
+
+            Console.WriteLine("Greeting:" + reply.Status);
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
