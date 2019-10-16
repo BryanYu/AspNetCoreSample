ecd332170616d6d9b3f5d3635cd332b50002d80b
BryanYu <d58526@gmail.com>
2019-10-16 15:46:01 +0800

    EF Core OrderBy

--- AspNetCoreSample/RazorPageWIthEFCore/Pages/Students/Index.cshtml.cs
+++ AspNetCoreSample/RazorPageWIthEFCore/Pages/Students/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -19,11 +20,41 @@ namespace RazorPageWIthEFCore.Pages.Students
             _context = context;
         }
 
-        public IList<Student> Student { get;set; }
+        public IList<Student> Students { get;set; }
 
-        public async Task OnGetAsync()
+        public string NameSort { get; set; }
+
+        public string DateSort { get; set; }
+
+        public string CurrentFilter { get; set; }
+
+        public string CurrentSort { get; set; }
+
+        public async Task OnGetAsync(string sortOrder)
         {
-            Student = await _context.Students.ToListAsync();
+            NameSort = string.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+
+            IQueryable<Student> studentIQ = from s in _context.Students select s;
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    studentIQ = studentIQ.OrderByDescending(s => s.LastName);
+                    break;
+                case "Date":
+                    studentIQ = studentIQ.OrderBy(s => s.EnrollmentDate);
+                    break;
+                case "date_desc":
+                    studentIQ = studentIQ.OrderByDescending(s => s.EnrollmentDate);
+                    break;
+                default:
+                    studentIQ = studentIQ.OrderBy(s => s.LastName);
+                    break;
+            }
+
+            Students = await studentIQ.AsNoTracking().ToListAsync();
         }   
     }
 }
