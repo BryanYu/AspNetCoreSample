e00dabbe637f1ec3e666c75e5731cc89af91c945
BryanYu <d58526@gmail.com>
2019-10-28 18:05:29 +0800

    Swagger Sample Init

--- AspNetCoreSample/AspNetCoreWebAPI/Controllers/LogController.cs
+++ AspNetCoreSample/AspNetCoreWebAPI/Controllers/LogController.cs
@@ -2,20 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace AspNetCoreWebAPI.Controllers
 {
+    /// <summary>
+    /// Controller
+    /// </summary>
     [Route("api/[controller]")]
-    public class LogController : Controller
+    [ApiController]
+    public class LogController : ControllerBase
     {
-        // GET: api/<controller>
+        
+        /// <summary>
+        /// 測試
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
-        public string Get()
+        public IActionResult Get()
         {
-            return "OK";
+            return NotFound();
         }
     }
 }
